24547a2e266091ce73230e1625c072d5d659f015
AlfarLatypov <33726768+AlfarLatypov@users.noreply.github.com>
2018-05-26 23:47:31 +0600

    Добавьте файлы проекта.

---
+++ XML_NAIM_PRICE_TOVAR_BENZINE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_NAIM_PRICE_TOVAR_BENZINE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
